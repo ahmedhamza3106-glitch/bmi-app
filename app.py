d9f35d9201b85cab90be44f830d478cacf9d38c4
Ahmed Hamza <ahmedhamza3106@gmail.com>
2026-03-29 07:52:31 +0200

    Update BMI logic and app layout

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from logic import calculate_bmi_logic
 
 st.title("حاسبة مؤشر كتلة الجسم 🩺")
 
-w = st.number_input("الوزن (kg):", value=70.0)
-l = st.number_input("الطول (cm):", value=170.0)
-
+w = st.number_input("الوزن (kg):", value= 67.0, step= 1.0)
+l = st.number_input("الطول (cm):", value= 170.0, step= 1.0)
 if st.button("احسب الآن"):
    
     bmi_val, category = calculate_bmi_logic(w, l)
---
+++ test_try.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = [190, 190, 120, 49]
+s = pd.Series(data)
+
+print(s)
+
